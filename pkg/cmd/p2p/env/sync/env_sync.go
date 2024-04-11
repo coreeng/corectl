81e4735793d5d5d3ae2eed37257614c6cc5c80ec
Peter Taylor <peter.taylor@cecg.io>
2024-04-11 09:02:59 +0100

    Refactoring post review

--- pkg/cmd/p2p/env/sync/env_sync.go
+++ pkg/cmd/p2p/env/sync/env_sync.go
@@ -8,10 +8,10 @@ import (
 
 	"github.com/coreeng/corectl/pkg/cmdutil/config"
 	"github.com/coreeng/corectl/pkg/cmdutil/userio"
+	"github.com/coreeng/corectl/pkg/environment"
 	"github.com/coreeng/corectl/pkg/git"
 	"github.com/google/go-github/v59/github"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
 type EnvCreateOpts struct {
@@ -68,7 +68,7 @@ func NewP2PSyncCmd(cfg *config.Config) (*cobra.Command, error) {
 		"apprepo",
 		"a",
 		"",
-		"Application Repo")
+		"Application Repository")
 	err = syncEnvironmentsCmd.MarkFlagRequired("apprepo")
 	if err != nil {
 		return nil, err
@@ -80,68 +80,32 @@ func NewP2PSyncCmd(cfg *config.Config) (*cobra.Command, error) {
 		false,
 		"Disable interactive inputs")
 
+	config.RegisterStringParameterAsFlag(
+		&cfg.Repositories.CPlatform,
+		syncEnvironmentsCmd.Flags())
+
 	return syncEnvironmentsCmd, nil
 }
 
 func run(opts *EnvCreateOpts, cfg *config.Config) error {
 
-	type domainItem struct {
-		Name   string `yaml:"name"`
-		Domain string `yaml:"domain"`
-	}
-	type ingressDomains struct {
-		DomainItem []domainItem `yaml:"ingressDomains,flow"`
-	}
-
-	type envConfig struct {
-		Platform struct {
-			ProjectId     string `yaml:"projectId"`
-			ProjectNumber string `yaml:"projectNumber"`
-		} `yaml:"platform"`
-		InternalServices struct {
-			Domain string `yaml:"domain"`
-		} `yaml:"internalServices"`
-		IngressDomains []domainItem `yaml:"ingressDomains"`
-	}
-
-	repositoriesDir := opts.RepositoriesDir
-	if repositoriesDir == "" {
-		configPath, err := config.Path()
-		if err != nil {
-			return err
-		}
-		configPath = filepath.Dir(configPath)
-		repositoriesDir = filepath.Join(configPath, "repositories")
-	}
-	if err := os.MkdirAll(repositoriesDir, 0o755); err != nil {
-		return err
-	}
 	githubClient := github.NewClient(nil).
 		WithAuthToken(cfg.GitHub.Token.Value)
-	gitAuth := git.UrlTokenAuthMethod(cfg.GitHub.Token.Value)
 
-	dplatformRepoFullname, err := git.DeriveRepositoryFullnameFromUrl(opts.DPlatformRepo)
-
-	// Clone or Reset DPlatform Repo
-	clonedRepository, err := cloneRepository(opts.Streams, gitAuth, githubClient, repositoriesDir, dplatformRepoFullname)
+	envs, err := environment.List(cfg.Repositories.CPlatform.Value)
 	if err != nil {
 		return err
 	}
 
-	// Read Existing Config
-	configFile := filepath.Join(repositoriesDir, dplatformRepoFullname.Name, "environments", opts.Name, "config.yaml")
-	fileBytes, err := os.ReadFile(configFile)
-	if err != nil {
-		return err
+	var requestedEnv environment.Environment
+	for _, requestedEnv = range envs {
+		if string(requestedEnv.Environment) == opts.Name {
+			break
+		}
 	}
 
-	var envC envConfig
-	err = yaml.Unmarshal(fileBytes, &envC)
-	if err != nil {
-		return err
-	}
-	var domain domainItem
-	for _, domain = range envC.IngressDomains {
+	var domain environment.Domain
+	for _, domain = range requestedEnv.IngressDomains {
 		if domain.Name == "default" {
 			break
 		}
@@ -153,7 +117,7 @@ func run(opts *EnvCreateOpts, cfg *config.Config) error {
 		},
 		{
 			Name:  "INTERNAL_SERVICES_DOMAIN",
-			Value: envC.InternalServices.Domain,
+			Value: requestedEnv.InternalServices.Domain,
 		},
 		{
 			Name:  "DPLATFORM",
@@ -161,16 +125,15 @@ func run(opts *EnvCreateOpts, cfg *config.Config) error {
 		},
 		{
 			Name:  "PROJECT_ID",
-			Value: envC.Platform.ProjectId,
+			Value: requestedEnv.Platform.ProjectId,
 		},
 		{
 			Name:  "PROJECT_NUMBER",
-			Value: envC.Platform.ProjectNumber,
+			Value: requestedEnv.Platform.ProjectNumber,
 		},
 	}
 	_ = varsToCreate
 
-	opts.Streams.Info("Repository local path " + clonedRepository.dplatform.Path())
 	opts.Streams.Info("Creating Environment...")
 	environments, _, err := githubClient.Repositories.CreateUpdateEnvironment(
 		context.Background(),
@@ -190,29 +153,25 @@ func run(opts *EnvCreateOpts, cfg *config.Config) error {
 	opts.Streams.Info("Repo: " + *repository.Name)
 
 	for i := range varsToCreate {
-		opts.Streams.Info("Removing var: " + varsToCreate[i].Name)
-		response, err := githubClient.Actions.DeleteEnvVariable(
-			context.Background(),
-			int(repository.GetID()),
-			*environments.Name,
-			varsToCreate[i].Name)
-		if err != nil {
-			if response.StatusCode == 404 {
-				opts.Streams.Info("Skipping " + varsToCreate[i].Name + ", not present")
-			} else {
-				return err
-			}
-		}
-		opts.Streams.Info("Creating Var: " + varsToCreate[i].Name)
 
-		_, err = githubClient.Actions.CreateEnvVariable(
+		response, err := githubClient.Actions.CreateEnvVariable(
 			context.Background(),
 			int(repository.GetID()),
 			*environments.Name,
 			&varsToCreate[i],
 		)
 		if err != nil {
-			return err
+			if response.StatusCode == 409 {
+				// Variable exists, we need to call update.
+				_, err := githubClient.Actions.UpdateEnvVariable(
+					context.Background(),
+					int(repository.GetID()),
+					*environments.Name,
+					&varsToCreate[i])
+				if err != nil {
+					return err
+				}
+			}
 		}
 	}
 
